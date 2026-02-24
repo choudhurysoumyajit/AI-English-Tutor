18ebcfcb43d7e1243640ce264caa48a894ccff72
Soumyajit Choudhury <choudhurysoumyajit@gmail.com>
2026-02-24 22:10:31 +0530

    Add files via upload

--- ui.py
+++ ui.py
@@ -6,8 +6,30 @@ import base64
 import os
 import sys
 
+
+
 # Initialize floating features for the interface
 float_init()
+
+##############
+
+page_element="""
+<style>
+[data-testid="stAppViewContainer"]{
+  background-image: url("https://cdn.wallpapersafari.com/88/75/cLUQqJ.jpg");
+  background-size: cover;
+}
+[data-testid="stHeader"]{
+  background-color: rgba(0,0,0,0);
+}
+</style>
+"""
+st.markdown(page_element, unsafe_allow_html=True)
+
+#Styling the Delete Chat Button
+st.markdown( """ <style> div.stButton > button:first-child { background-color: #e74c3c; color: white; border-radius: 8px; height: 50px; width: 100%; font-size: 18px; } div.stButton > button:hover { background-color: #c0392b; color: white; } </style> """, unsafe_allow_html=True )
+
+#############
 
 def autoplay_audio(file_path: str):
     with open(file_path, "rb") as f:
@@ -32,19 +54,13 @@ def initialize_session_state():
 initialize_session_state()
 
 st.set_page_config(page_title="Advance Rag", layout="wide")
-st.title(":material/robot_2: THE BOT ENGLISH TUTOR ")
-st.markdown(":green[Click Record button from sidebar to start the chat. Click again to stop.]")
+#st.title(":material/robot_2: THE BOT ENGLISH TUTOR ")
+st.markdown("<h1 style='text-align: center; color: white';>🤖 THE BOT ENGLISH TUTOR</h1>", unsafe_allow_html=True)
+st.markdown("<h6 style='text-align: center; color: lime';>Click on Record Button to start. Click again to stop.</h6>", unsafe_allow_html=True)
 
-st.sidebar.image("Logo.png")
-
-colspace,col1, col2 = st.sidebar.columns([1,1,1])
-
-with colspace:
-    # This column is intentionally left blank
-    pass 
-
-with col2:
-    if st.sidebar.button("Clear Chat",key="clear_chat_btn",width="stretch"):
+button_container = st.container()
+with button_container:
+    if st.button("Clear Chat",key="clear_chat_btn",width="stretch"):
         st.session_state.messages = [
             {"role": "assistant", "content": "Hi! How may I assist you today?"}
         ]
@@ -55,13 +71,20 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-with col1:
     #footer_container = st.sidebar.container()
 #with footer_container:
-    audio_bytes = audio_recorder(text="",recording_color="#e74c3c",
-    neutral_color="#3498db",    
-    icon_size="3x", pause_threshold=10.0, sample_rate=41_000)
-    
+with button_container:
+    # Create three columns inside the container col_left, col_center, col_right = st.columns([1,2,1])
+    col_left, col_center, col_right = st.columns([1,0.112,1])
+    with col_center:
+        # Show transparent mic icon 
+        audio_bytes = audio_recorder(text="",
+        recording_color="#e74c3c",
+        neutral_color="#3498db",    
+        icon_size="3x", pause_threshold=10.0, sample_rate=41_000)
+
+# Float the container to the bottom center
+button_container.float("bottom: 0.5rem; display: flex; justify-content: center; background-color: transparent; border: none; box-shadow: none;") 
 
 if audio_bytes:
     with st.spinner("Transcribing..."):
